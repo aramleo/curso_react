113f5c47c45a0559b3782a96149ae99806990ef8
aramleo <anrale@gmail.com>
2021-12-07 23:29:35 +0100

    cuarto

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <div>Este es un curso de React principiantes</div>,
-    document.getElementById("root")
-)
+  <div>Este es un curso de React principiantes</div>,
+  document.getElementById('root'),
+);
